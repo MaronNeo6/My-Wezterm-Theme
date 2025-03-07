46aaf5ade3d56b5d0cf18a35cf3c773f49efd6e2
MaronNeo6 <eduardodantasnery@gmail.com>
2025-03-06 21:40:55 -0300

    modified: wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -8,7 +8,7 @@ end
 
 -- Configuração Básica
 
-config.color_scheme = "Abernathy"
+config.color_scheme = "Andromeda"
 config.scrollback_lines = 3000
 config.default_workspace = "home"
 config.window_background_opacity = 0.9
